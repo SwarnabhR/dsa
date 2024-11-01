b25b5b1bbcd886be339c237d28c159a130f83810
Swarnabh Roy <workspace.swarnabh@gmail.com>
2024-11-01 12:15:43 +0530

    Commit new changes

--- geeksforgeeks/arraySubset.cpp
+++ geeksforgeeks/arraySubset.cpp
@@ -4,24 +4,32 @@ using namespace std;
 
 string isSubset(int a1[], int a2[], int n, int m)
 {
+    // Create a map to store the values of first array and their counts
     map<int, int> mpp;
 
+    // Iterate over the first array and fill the map
     for (int i = 0; i < n; i++)
     {
+        // Increment the count of the current element
         mpp[a1[i]]++;
     }
 
+    // Iterate over the second array and check if it's a subset of the first array
     for (int j = 0; j < m; j++)
     {
+        // If the current element is present in the map and its count is not zero
         if (mpp.find(a2[j]) != mpp.end() && mpp[a2[j]] != 0)
         {
+            // Decrement its count
             mpp[a2[j]]--;
         }
         else
         {
+            // If the element is not present or its count is zero, return "No"
             return "No";
         }
     }
+    // If we have iterated over the entire second array and didn't find any element that is not present in the first array, return "Yes"
     return "Yes";
 }
 
